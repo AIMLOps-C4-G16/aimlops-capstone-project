dd3f96decf1081049c1d4b14141a85844b7c57f6
Abhinand Menon <abhinandmenon@users.noreply.github.com>
2025-07-06 02:48:16 +0000

    use tempfile for uploaded image

--- api/server.py
+++ api/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.templating import Jinja2Templates
 import base64
+import tempfile
 
 from inference import generate_caption
 
@@ -15,15 +16,14 @@ def dynamic_file(request: Request):
 @app.post("/dynamic")
 def dynamic(request: Request, file: UploadFile = File()):
     data = file.file.read()
-    # Image will be saved in the uploads folder prefixed with uploaded_
-    image_file = "uploads/saved_" + file.filename
-    with open(image_file, "wb") as f:
-        f.write(data)
+    caption = ''
+    with tempfile.NamedTemporaryFile() as tmp:
+        tmp.write(data)
+        caption = generate_caption(tmp.name)
     file.file.close()
 
     # encoding and decoding the image bytes
     encoded_image = base64.b64encode(data).decode("utf-8")
-    caption = generate_caption(image_file)
 
     return templates.TemplateResponse(
         "dynamic.html", {"request": request,  "img": encoded_image, "caption": caption})
