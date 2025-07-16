aa0ccc5018ae12417f876df9382155814255ca6f
Abhinand Menon <abhinandmenon@users.noreply.github.com>
2025-07-16 09:00:10 +0000

    changed all query params to multipart form data fields and added html search endpoints

--- api/ic_model_api/search.py
+++ api/ic_model_api/search.py
@@ -1,11 +1,14 @@
 import tempfile
+from typing import Annotated
 
-from fastapi import APIRouter, Request, UploadFile, File
+from fastapi import APIRouter, Request, UploadFile, File, Form
+from fastapi.templating import Jinja2Templates
 
 from config import settings
 
 
 search_router = APIRouter()
+templates = Jinja2Templates(directory=settings.TEMPLATES_DIRECTORY)
 
 
 def caption_image(image: UploadFile):
@@ -17,12 +20,37 @@ def caption_image(image: UploadFile):
     return caption
 
 
-@search_router.get("/search")
-def search(request: Request, text: str, num: int = 3):
+@search_router.post("/search")
+def search(request: Request, text: Annotated[str, Form()], num: Annotated[int, Form()] = 3):
     return settings.IMAGE_DB_INDEX[0].search(text, num)
 
 
+@search_router.get("/search_page")
+def search_home(request: Request):
+    return templates.TemplateResponse("search_form.html", {"request": request})
+
+
+@search_router.post("/search_page")
+def search_page(request: Request, text: Annotated[str, Form()], num: Annotated[int, Form()] = 3):
+    imgs_list = settings.IMAGE_DB_INDEX[0].search(text, num)
+    return templates.TemplateResponse(
+        "search_form.html", {"request": request,  "imgs_list": imgs_list})
+
+
 @search_router.post("/search_similar")
-def search_similar(request: Request, image: UploadFile = File(), num: int = 3):
+def search_similar(request: Request, image: UploadFile = File(), num: Annotated[int, Form()] = 3):
     caption = caption_image(image)
     return settings.IMAGE_DB_INDEX[0].search(caption, num)
+
+
+@search_router.get("/search_similar_page")
+def search_similar_home(request: Request):
+    return templates.TemplateResponse("search_similar_form.html", {"request": request})
+
+
+@search_router.post("/search_similar_page")
+def search_similar_page(request: Request, image: UploadFile = File(), num: Annotated[int, Form()] = 3):
+    caption = caption_image(image)
+    imgs_list = settings.IMAGE_DB_INDEX[0].search(caption, num)
+    return templates.TemplateResponse(
+        "search_similar_form.html", {"request": request,  "imgs_list": imgs_list, "caption": caption})
